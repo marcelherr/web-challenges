fbb29057d6be616b7c74301d4b304e4debb421a1
Marcel Herr <mrclhrr@gmail.com>
2024-03-07 15:02:34 +0100

    dark mode challenge completed

--- js-basics_dark-mode/js/index.js
+++ js-basics_dark-mode/js/index.js
@@ -1,6 +1,18 @@
 console.clear();
 
-const bodyElement = document.querySelector('[data-js="dark-mode-button]');
-button.addEventlistener("click", () => {
-  bodyElement.classList.add("body.dark");
+const body = document.querySelector('[data-js="body"]');
+const darkModeButton = document.querySelector('[data-js="dark-mode-button"]');
+const ligthModeButton = document.querySelector('[data-js="light-mode-button"]');
+const toggleButton = document.querySelector('[data-js="toggle-button"]');
+
+darkModeButton.addEventListener("click", () => {
+  body.classList.add("dark");
+});
+
+ligthModeButton.addEventListener("click", () => {
+  body.classList.remove("dark");
+});
+
+toggleButton.addEventListener("click", () => {
+  body.classList.toggle("dark");
 });
