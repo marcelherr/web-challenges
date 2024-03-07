c83b2afd74bf8cc2223b4e7a60747fce36c6ebbe
Marcel Herr <mrclhrr@gmail.com>
2024-03-07 16:14:14 +0100

    challenge completed

--- js-conditions-and-booleans_teenager-check/js/index.js
+++ js-conditions-and-booleans_teenager-check/js/index.js
@@ -1,3 +1,5 @@
+console.clear();
+
 const input = document.querySelector('[data-js="age-input"]');
 const button = document.querySelector('[data-js="age-button"]');
 const output = document.querySelector('[data-js="age-output"]');
@@ -10,4 +12,10 @@ button.addEventListener("click", () => {
   // Exercise:
   // Use conditions and the logical AND operator to write
   // "You are a teen." or "You are not a teen." into the output.
+
+  if (age >= 13 && age <= 19) {
+    console.log("You are a teen.");
+  } else {
+    console.log("You are not a teen.");
+  }
 });
