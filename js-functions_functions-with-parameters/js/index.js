39c64170632316eb7774755deba5938393ac1116
Marcel Herr <mrclhrr@gmail.com>
2024-03-08 11:29:07 +0100

    challenge solved

--- js-functions_functions-with-parameters/js/index.js
+++ js-functions_functions-with-parameters/js/index.js
@@ -1,6 +1,6 @@
 console.clear();
 
-console.log("-------------");
+logSeparator();
 console.log("Exercise 1:");
 /*
 1: The function below is not executed. Can you figure out why? Find the bug and fix it.
@@ -12,36 +12,27 @@ console.log("Exercise 1:");
 function logText() {
   console.log("This text does not appear. Why?");
 }
+logText();
 
-console.log("-------------");
+logSeparator();
 console.log("Exercise 2:");
 /*
 2: Look at the functions below. They are almost identical. Can you find a way to generalize them into a new function with an input parameter? Replace the function calls below with your new function.
 */
 
-function greetBob() {
-  console.log("Welcome Bob, good to see you again!");
-}
-
-function greetAlice() {
-  console.log("Welcome Alice, good to see you again!");
-}
-
-function greetMary() {
-  console.log("Welcome Mary, good to see you again!");
-}
-
-greetAlice();
-greetBob();
-greetMary();
-
 // --v-- write your code here --v--
 
-function greet() {}
+function greetUser(user) {
+  console.log(`Welcome ${user}, good to see you again!`);
+}
+
+greetUser("Alice");
+greetUser("Bob");
+greetUser("Mary");
 
 // --^-- write your code here --^--
 
-console.log("-------------");
+logSeparator();
 console.log("Exercise 3:");
 /*
 3: We log a little separation text for each subtask. This is repetitive code, so lets write a function 'logSeparator' which takes the number of the exercise as a parameter and logs the respective separator. Then use this function to replace the existing separator logs in this javascript file.
@@ -50,5 +41,8 @@ Note: Separation text means the text that is logged into the console before each
 */
 
 // --v-- write your code here --v--
-
+function logSeparator() {
+  console.log("-------------Banana Clip");
+}
+logSeparator();
 // --^-- write your code here --^--
