64ebab8efa3079275458d2d2f4e4c42f32a77290
Marcel Herr <mrclhrr@gmail.com>
2024-03-08 18:03:53 +0100

    x

--- js-functions_pizza-analyzer/js/index.js
+++ js-functions_pizza-analyzer/js/index.js
@@ -26,3 +26,9 @@ function calculatePizzaGain() {}
 
 // Task 3
 // define the function updateOutputColor here
+
+// eventuell hilft das
+// button.addEventListener("click", () => {
+//   // Number(x) converts the string from the input to a number
+//   // Beware: input values are *always* strings!
+//   const age = Number(input.value);
