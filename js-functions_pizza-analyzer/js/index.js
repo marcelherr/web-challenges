222732fcfecf486f9522b7bacc04be6d5ea1a252
Marcel Herr <mrclhrr@gmail.com>
2024-03-11 14:01:08 +0100

    solved challenge and working on pizza challenge

--- js-functions_pizza-analyzer/js/index.js
+++ js-functions_pizza-analyzer/js/index.js
@@ -9,23 +9,39 @@ const outputSection = document.querySelector('[data-js="output-section"]');
 const output = document.querySelector('[data-js="output"]');
 
 pizzaInput1.addEventListener("input", () => {
-  // write your code here
+  const pizzaSize1 = pizzaInput1.value;
+  return pizzaSize1;
 });
 
 pizzaInput2.addEventListener("input", () => {
-  // write your code here
+  const pizzaSize2 = pizzaInput2.value;
+  return pizzaSize2;
 });
 
 // Task 1
 // define the function calculatePizzaGain here
 
-function calculatePizzaGain() {}
+function calculatePizzaGain(diameter1, diameter2) {
+  const area1 = (diameter1 / 2) ** 2 * Math.PI;
+  const area2 = (diameter2 / 2) ** 2 * Math.PI;
+  const pizzaPercentage = Math.round(((area2 - area1) / area1) * 100);
+  return pizzaPercentage;
+}
 
 // Task 2
 // define the function updatePizzaDisplay here
 
+function updatePizzaDisplay(pizzaElement, newSize) {
+  const newDisplaySize = (newSize / 24) * 100;
+  pizzaElement.style.width = newDisplaySize + "px";
+}
+
 // Task 3
 // define the function updateOutputColor here
+
+function updateOutputColor(size1, size2) {
+  return;
+}
 
 // eventuell hilft das
 // button.addEventListener("click", () => {
