a735560847031534d3f09ca450dd4a88ecf25e1f
Marcel Herr <mrclhrr@gmail.com>
2024-03-12 19:35:09 +0100

    add files

---
+++ js-forms_handle-form-submit/js/index.js
@@ -0,0 +1,13 @@
+console.clear();
+
+const form = document.querySelector('[data-js="form"]');
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+  const formElements = event.target.elements;
+  const formData = new FormData(event.target);
+  const data = Object.fromEntries(formData);
+  console.log(data);
+  formElements.firstName.focus();
+  event.target.reset();
+});
--- js-functions_pizza-analyzer/js/index.js
+++ js-functions_pizza-analyzer/js/index.js
@@ -10,11 +10,17 @@ const output = document.querySelector('[data-js="output"]');
 
 pizzaInput1.addEventListener("input", () => {
   const pizzaSize1 = pizzaInput1.value;
+  calculatePizzaGain;
+  updateOutputColor;
+  updatePizzaDisplay;
   return pizzaSize1;
 });
 
 pizzaInput2.addEventListener("input", () => {
   const pizzaSize2 = pizzaInput2.value;
+  calculatePizzaGain;
+  updateOutputColor;
+  updatePizzaDisplay;
   return pizzaSize2;
 });
 
@@ -25,6 +31,7 @@ function calculatePizzaGain(diameter1, diameter2) {
   const area1 = (diameter1 / 2) ** 2 * Math.PI;
   const area2 = (diameter2 / 2) ** 2 * Math.PI;
   const pizzaPercentage = Math.round(((area2 - area1) / area1) * 100);
+  console.log(pizzaPercentage);
   return pizzaPercentage;
 }
 
@@ -33,18 +40,17 @@ function calculatePizzaGain(diameter1, diameter2) {
 
 function updatePizzaDisplay(pizzaElement, newSize) {
   const newDisplaySize = (newSize / 24) * 100;
-  pizzaElement.style.width = newDisplaySize + "px";
+  pizzaElement.style.width = `${newDisplaySize}"px"`;
 }
 
 // Task 3
 // define the function updateOutputColor here
 
 function updateOutputColor(size1, size2) {
-  return;
+  if (size1 >= size2) {
+    outputSection = "var(--green)";
+  } else {
+    outputSection = "var(--red)";
+  }
 }
-
-// eventuell hilft das
-// button.addEventListener("click", () => {
-//   // Number(x) converts the string from the input to a number
-//   // Beware: input values are *always* strings!
-//   const age = Number(input.value);
+console.log(updateOutputColor());
