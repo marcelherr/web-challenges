2b3c9954b707f784e33b5d464aa5c66e8048aadc
Marcel Herr <mrclhrr@gmail.com>
2024-03-18 16:33:06 +0100

    add js fetch challenge

---
+++ js-fetch_star-wars-console/js/index.js
@@ -0,0 +1,11 @@
+console.clear();
+
+const url = "https://swapi.dev/api/people";
+
+async function fetchData() {
+  const response = await fetch(url);
+  const data = await response.json();
+  console.log(data.results[2].eye_color);
+}
+
+fetchData();
---
+++ js-fetch_star-wars-console/js/utils.js
@@ -0,0 +1,5 @@
+export const root = document.getElementById("root");
+
+export function renderElement(element) {
+  root.appendChild(element); // Append the Element to the page
+}
