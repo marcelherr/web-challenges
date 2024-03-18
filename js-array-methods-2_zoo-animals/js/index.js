e0ed128cc297b3b0963380b6a78b620ea468cb31
Marcel Herr <mrclhrr@gmail.com>
2024-03-18 13:02:06 +0100

    add challenge files

---
+++ js-array-methods-2_zoo-animals/js/index.js
@@ -0,0 +1,58 @@
+import { zooAnimals } from "../utils/db.js";
+
+// This website uses a function to check wether a specific animal is part of the zoo or not but it's not working correct.
+// Please implement the function 'hasAnimal' that gets an array of animals and an animal name
+// The function should return 'true' if the animal name is included in the array or 'false' if not.
+
+// function hasAnimal(animals, animalName) {
+//   const checkAnimal = animals.includes(animalName);
+//   return checkAnimal;
+// }
+
+function hasAnimal(animals, animalName) {
+  const lowerCaseAnimals = animals.map((animal) => animal.toLowerCase());
+  const lowerCaseAnimalName = animalName.toLowerCase();
+  const checkAnimal = lowerCaseAnimals.includes(lowerCaseAnimalName);
+  return checkAnimal;
+}
+
+// function hasAnimal(animals, animalName) {
+//   animalName = animalName.toLowerCase();
+//   return animals.some((animal) => animal.toLowerCase() === animalName);
+// }
+
+// const pets = ["bird", "cat", "dog", "ferret", "fish"];
+// const uppercasePets = pets.map((pet) => {
+//   return pet.toUpperCase();
+// });
+
+// Bonus:
+// Can you modify the function so that it works case-insensitive?
+// This means it should return true, even if you search for 'sheep' or 'SHEEP' or 'sHeEp'.
+// Hint: Remember the 'map' function
+
+// -------------------------------------------------------------------------------------
+// ----- The following code is used for the browser preview. Please don't touch it -----
+
+const animalList = document.querySelector("[data-js='animalList']");
+const animalForm = document.querySelector("[data-js='animalForm']");
+const output = document.querySelector("[data-js='output']");
+
+animalForm.addEventListener("submit", (event) => {
+  event.preventDefault();
+  const searchQuery = event.target.searchQuery.value;
+  output.textContent = "";
+  if (searchQuery.trim() === "") {
+    return;
+  }
+  const result = hasAnimal(zooAnimals, searchQuery);
+  output.textContent = result
+    ? `Yes, we have ${searchQuery}`
+    : `No, we don't have ${searchQuery}`;
+});
+zooAnimals.forEach((animal) => {
+  const tag = document.createElement("span");
+  tag.classList.add("tag");
+  tag.textContent = animal;
+  animalList.append(tag);
+});
