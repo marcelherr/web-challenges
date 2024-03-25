55d92957ecf8e991147ef069e47736580610de86
Marcel Herr <mrclhrr@gmail.com>
2024-03-25 16:37:56 +0100

    add react props sum

---
+++ react-props_sum/src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./styles.css";
+
+export default function App() {
+  return <Sum valueA={5} valueB={2} />;
+}
+
+function Sum({ valueA, valueB }) {
+  return (
+    <h1>
+      {`${valueA} + ${valueB} = `}
+      {valueA + valueB}
+    </h1>
+  );
+}
