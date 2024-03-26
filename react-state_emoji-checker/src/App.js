38a997637d6c38490a981b9a0c176bedf325a72c
Marcel Herr <mrclhrr@gmail.com>
2024-03-26 15:58:27 +0100

    add react state emoji checker challenge

---
+++ react-state_emoji-checker/src/App.js
@@ -0,0 +1,66 @@
+import React from "react";
+import "./styles.css";
+import { useState } from "react";
+
+export default function App() {
+  const [code, setCode] = useState("");
+  const validCode = "🐡🐠🐋";
+
+  function handleClick() {
+    setCode(code + event.target.textContent);
+  }
+  return (
+    <div className="container">
+      <div className="button-container">
+        <button
+          type="button"
+          onClick={() => {
+            handleClick();
+            console.log("Update Code!");
+            console.log(code);
+          }}
+        >
+          <span role="img" aria-label="Pufferfish">
+            🐡
+          </span>
+        </button>
+        <button
+          type="button"
+          onClick={() => {
+            handleClick();
+            console.log("Update Code!");
+            console.log(code);
+          }}
+        >
+          <span role="img" aria-label="Whale">
+            🐋
+          </span>
+        </button>
+        <button
+          type="button"
+          onClick={() => {
+            handleClick();
+            console.log("Update Code!");
+            console.log(code);
+          }}
+        >
+          <span role="img" aria-label="Clownfish">
+            🐠
+          </span>
+        </button>
+      </div>
+
+      <button
+        type="button"
+        onClick={() => {
+          setCode("");
+          console.log("Reset Code!");
+        }}
+      >
+        Reset
+      </button>
+
+      {code === validCode && <p>Valid code!</p>}
+    </div>
+  );
+}
