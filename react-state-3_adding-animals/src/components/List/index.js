6c69e99cc1d19c4dc676ab30a18b114e679acb88
Marcel Herr <mrclhrr@gmail.com>
2024-03-28 12:08:07 +0100

    add react state 3 adding animals challenge

---
+++ react-state-3_adding-animals/src/components/List/index.js
@@ -0,0 +1,21 @@
+import "./List.css";
+
+export default function List({ animals }) {
+  // console.log(animals.name);
+  // console.log(animals.id);
+  return (
+    <>
+      <h2>Your animals:</h2>
+      <ul className="animal-list">
+        {animals.map((animal) => (
+          <li key={animal.id} className="animal-list__item">
+            <h3>{animal.name}</h3>
+            <span aria-label="emoji" className="animal-list__emoji">
+              {animal.emoji}
+            </span>
+          </li>
+        ))}
+      </ul>
+    </>
+  );
+}
