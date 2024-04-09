4b575fb1f7191932b6d3675765f8867840f70074
Marcel Herr <mrclhrr@gmail.com>
2024-04-09 16:52:28 +0200

    solved challenge next.js dynamic routes

---
+++ nextjs-dynamic-routes_lotr-app-dynamic-routes/pages/volumes/[slug].js
@@ -0,0 +1,41 @@
+import Image from "next/image";
+import Link from "next/link";
+import { volumes } from "../../lib/data";
+import { useRouter } from "next/router";
+import Head from "next/head";
+
+export default function VolumeDetail() {
+  const router = useRouter();
+  const { slug } = router.query;
+  const currentVolume = volumes.find((volume) => volume.slug === slug);
+
+  if (!currentVolume) {
+    return null;
+  }
+  const { title, description, cover, books } = currentVolume;
+
+  return (
+    <>
+      {" "}
+      <Head>
+        <title>{title}</title>
+      </Head>
+      <Link href="/volumes">← All Volumes</Link>
+      <h1>{title}</h1>
+      <p>{description}</p>
+      <ul>
+        {books.map(({ ordinal, title }) => (
+          <li key={title}>
+            {ordinal}: <strong>{title}</strong>
+          </li>
+        ))}
+      </ul>
+      <Image
+        src={cover}
+        alt={`Cover image of ${title}`}
+        width={140}
+        height={230}
+      />{" "}
+    </>
+  );
+}
---
+++ nextjs-dynamic-routes_lotr-app-dynamic-routes/pages/volumes/index.js
@@ -0,0 +1,35 @@
+import Link from "next/link";
+import { introduction, volumes } from "../../lib/data";
+import { useRouter } from "next/router";
+import Head from "next/head";
+
+export default function Volumes() {
+  const router = useRouter();
+
+  // function getRandomElement(array) {
+  //   return array[Math.floor(Math.random() * array.length)];
+  // }
+  // function handleRandomCLick() {
+  //   const randomValue = getRandomElement();
+  //   // router.push(`/${randomValue}`);
+  //   console.log(randomValue);
+  // }
+
+  return (
+    <>
+      <Head>
+        <title>The Lord of the Rings</title>
+      </Head>
+      <h1>The Lord of the Rings</h1>
+      <p>{introduction}</p>
+      <ul>
+        {volumes.map(({ id, slug, title }) => (
+          <li key={id}>
+            <Link href={`/volumes/${slug}`}>{title}</Link>
+          </li>
+        ))}
+      </ul>
+      <button type="button">Get Random Volume</button>
+    </>
+  );
+}
