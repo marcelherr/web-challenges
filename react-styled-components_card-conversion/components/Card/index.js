965a831f07594a232f2f8b02599488e10036ce0b
Marcel Herr <mrclhrr@gmail.com>
2024-04-10 12:28:21 +0200

    refactor code

--- react-styled-components_card-conversion/components/Card/index.js
+++ react-styled-components_card-conversion/components/Card/index.js
@@ -1,22 +1,24 @@
 import styled from "styled-components";
 
-export default function Card() {
-  const StyledDiv = styled.div`
-    border-radius: 4px;
-    padding: 8px;
-    box-shadow: 0px 2px 1px -1px rgba(0, 0, 0, 0.2),
-      0px 1px 1px 0px rgba(0, 0, 0, 0.14), 0px 1px 3px 0px rgba(0, 0, 0, 0.12);
-  `;
-  const StyledCardTitle = styled.h3`
-    margin-top: 0;
-    margin-bottom: 12px;
-    font-size: 1.25rem;
-  `;
-  const StyledDescription = styled.p`
-    margin-top: 0;
-    margin-bottom: 4px;
-  `;
+const StyledDiv = styled.div`
+  border-radius: 4px;
+  padding: 8px;
+  box-shadow: 0px 2px 1px -1px rgba(0, 0, 0, 0.2),
+    0px 1px 1px 0px rgba(0, 0, 0, 0.14), 0px 1px 3px 0px rgba(0, 0, 0, 0.12);
+`;
 
+const StyledCardTitle = styled.h3`
+  margin-top: 0;
+  margin-bottom: 12px;
+  font-size: 1.25rem;
+`;
+
+const StyledDescription = styled.p`
+  margin-top: 0;
+  margin-bottom: 4px;
+`;
+
+export default function Card() {
   return (
     <StyledDiv>
       <StyledCardTitle>Homer Simpson</StyledCardTitle>
