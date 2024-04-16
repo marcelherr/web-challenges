e2b8c519c7d76f72176cfafe00ff5c56f956f8fc
Marcel Herr <mrclhrr@gmail.com>
2024-04-16 16:40:20 +0200

    implement bakend basics chance challenge solution

--- backend-basics_chance/server.js
+++ backend-basics_chance/server.js
@@ -1,3 +1,15 @@
 import { createServer } from "node:http";
 
-// export const server = …
+import { Chance } from "chance";
+
+const chance = new Chance();
+const name = chance.name();
+const age = chance.age();
+const profession = chance.profession();
+
+const output = `Hello, my name is ${name} and I am ${age} years old. I am a ${profession}.`;
+
+export const server = createServer((request, response) => {
+  response.statusCode = 200;
+  response.end(output);
+});
