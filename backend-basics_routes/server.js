e5a955aee16d3c916e03543f000af9019f42e802
Marcel Herr <mrclhrr@gmail.com>
2024-04-16 16:02:39 +0200

    implement challenge solution

--- backend-basics_routes/server.js
+++ backend-basics_routes/server.js
@@ -1,3 +1,20 @@
 import { createServer } from "node:http";
 
-// export const server = …
+export const server = createServer((request, response) => {
+  if (request.url === "/api/fish/1") {
+    const message = "Shrimp";
+    response.statusCode = 200;
+    response.end(message);
+    return;
+  }
+
+  if (request.url === "/api/fish/2") {
+    const message = "Anemonefish";
+    response.statusCode = 200;
+    response.end(message);
+    return;
+  }
+
+  response.statusCode = 404;
+  response.end("Not found");
+});
