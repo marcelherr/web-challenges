f7c41fe77292fe0b34723afa25b08fe538b06cfe
Marcel Herr <mrclhrr@gmail.com>
2024-04-19 16:59:43 +0200

    backend-update-and-delete_products

--- backend-update-and-delete_products/components/Product/index.js
+++ backend-update-and-delete_products/components/Product/index.js
@@ -3,12 +3,34 @@ import { useRouter } from "next/router";
 import { ProductCard } from "./Product.styled";
 import { StyledLink } from "../Link/Link.styled";
 import Comments from "../Comments";
+import { StyledButton } from "../Button/Button.styled";
+import { useState } from "react";
+import ProductForm from "../ProductForm";
 
 export default function Product() {
   const router = useRouter();
   const { id } = router.query;
 
-  const { data, isLoading } = useSWR(`/api/products/${id}`);
+  const { data, isLoading, mutate } = useSWR(`/api/products/${id}`);
+
+  const [isEditMode, setIsEditMode] = useState(false);
+
+  async function handleEditProduct(event) {
+    event.preventDefault();
+    const formData = new FormData(event.target);
+    const productData = Object.fromEntries(formData);
+
+    const response = await fetch(`/api/products/${id}`, {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(productData),
+    });
+    if (response.ok) {
+      mutate();
+    }
+  }
 
   if (isLoading) {
     return <h1>Loading...</h1>;
@@ -17,7 +39,14 @@ export default function Product() {
   if (!data) {
     return;
   }
-
+  async function handleDeleteProduct() {
+    const response = await fetch(`/api/products/${id}`, {
+      method: "DELETE",
+    });
+    if (response.ok) {
+      router.push("/");
+    }
+  }
   return (
     <ProductCard>
       <h2>{data.name}</h2>
@@ -26,7 +55,23 @@ export default function Product() {
         Price: {data.price} {data.currency}
       </p>
       {data.reviews.length > 0 && <Comments reviews={data.reviews} />}
+      <StyledButton
+        type="button"
+        onClick={() => {
+          setIsEditMode(!isEditMode);
+        }}
+      >
+        Edit
+      </StyledButton>
+      <StyledButton type="button" onClick={() => handleDeleteProduct(id)}>
+        Delete
+      </StyledButton>
       <StyledLink href="/">Back to all</StyledLink>
+      {isEditMode ? (
+        <ProductForm onSubmit={handleEditProduct} state={isEditMode} />
+      ) : (
+        <></>
+      )}
     </ProductCard>
   );
 }
--- backend-update-and-delete_products/components/ProductForm/index.js
+++ backend-update-and-delete_products/components/ProductForm/index.js
@@ -1,51 +1,51 @@
 import { StyledForm, StyledHeading, StyledLabel } from "./ProductForm.styled";
 import { StyledButton } from "../Button/Button.styled";
-import useSWR from "swr";
 
-export default function ProductForm() {
-  const { mutate } = useSWR("/api/products");
+const newFish = { name: "", description: "", price: 0, currency: "EUR" };
 
-  async function handleSubmit(event) {
-    event.preventDefault();
-
-    const formData = new FormData(event.target);
-    const productData = Object.fromEntries(formData);
-
-    const response = await fetch("/api/products", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(productData),
-    });
-
-    if (!response.ok) {
-      console.error(response.status);
-      return;
-    }
-
-    mutate();
-    event.target.reset();
-  }
-
+export default function ProductForm({
+  onSubmit,
+  state,
+  initalValues = newFish,
+}) {
   return (
-    <StyledForm onSubmit={handleSubmit}>
-      <StyledHeading>Add a new Fish</StyledHeading>
+    <StyledForm onSubmit={onSubmit}>
+      <StyledHeading>{state ? "Edit Fish" : "Add a new Fish"}</StyledHeading>
       <StyledLabel htmlFor="name">
         Name:
-        <input type="text" id="name" name="name" />
+        <input
+          type="text"
+          id="name"
+          name="name"
+          defaultValue={initalValues.name}
+        />
       </StyledLabel>
       <StyledLabel htmlFor="description">
         Description:
-        <input type="text" id="description" name="description" />
+        <input
+          type="text"
+          id="description"
+          name="description"
+          defaultValue={initalValues.description}
+        />
       </StyledLabel>
       <StyledLabel htmlFor="price">
         Price:
-        <input type="number" id="price" name="price" min="0" />
+        <input
+          type="number"
+          id="price"
+          name="price"
+          min="0"
+          defaultValue={initalValues.price}
+        />
       </StyledLabel>
       <StyledLabel htmlFor="currency">
         Currency:
-        <select id="currency" name="currency">
+        <select
+          id="currency"
+          name="currency"
+          defaultValue={initalValues.currency}
+        >
           <option value="EUR">EUR</option>
           <option value="USD">USD</option>
           <option value="GBP">GBP</option>
